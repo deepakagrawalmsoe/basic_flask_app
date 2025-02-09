08570bc6cc871a75caae85c8a61740314baadee7
deepakagrawalmsoe <138452263+deepakagrawalmsoe@users.noreply.github.com>
2025-02-09 18:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
 from flask import Flask
+from prometheus_client import make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
+REQUEST_COUNT = Counter('request_count', 'App Request Count')
 
 @app.route('/')
-def home():
-    return "Hello, OpenShift!"
+def hello():
+    REQUEST_COUNT.inc()
+    return "Hello from Flask!"
 
-if __name__ == "__main__":
+# Create a metrics endpoint
+metrics_app = make_wsgi_app()
+
+# Combine the Flask app and the metrics endpoint
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': metrics_app
+})
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # Use OpenShift's port
     app.run(host="0.0.0.0", port=port)
