ea200d93549f09f6ed0840365ddcaca79d81741d
deepakagrawalmsoe <138452263+deepakagrawalmsoe@users.noreply.github.com>
2025-02-11 02:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from flask import Flask
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
-REQUEST_COUNT = Counter('request_count', 'App Request Count')
+#REQUEST_COUNT = Counter('request_count', 'App Request Count')
 
 @app.route('/')
 def hello():
-    REQUEST_COUNT.inc()
+    #REQUEST_COUNT.inc()
     return "Hi there from Flask!"
 
-# Create a metrics endpoint
-##metrics_app = make_wsgi_app()
+## Create a metrics endpoint
+#metrics_app = make_wsgi_app()
 
-# Combine the Flask app and the metrics endpoint
-##app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-##    '/metrics': metrics_app
-##})
+## Combine the Flask app and the metrics endpoint
+#app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+#    '/metrics': metrics_app
+#})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # Use OpenShift's port
