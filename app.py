921f595c75fa53746d083d4ef9cd2a8f35b9f6ff
deepakagrawalmsoe <138452263+deepakagrawalmsoe@users.noreply.github.com>
2025-03-01 17:05:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,4 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # Use OpenShift's port
     app.run(host="0.0.0.0", port=port)
+
